ff0abb3d38562df343f2d637a1947d022be1404e
Franz BOCCO <franzbocco12@gmail.com>
2025-03-23 18:17:27 +0100

    Ajout d'indice pour aider l'utilisateur

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ def hazard ():
 a=hazard()
 b=""
 D=10
-while b=="" :
-    print ("DEVINEZ UNE VALEUR COMPRISE ENTRE 1 ET 101")
-    break
+print ("DEVINEZ UNE VALEUR COMPRISE ENTRE 1 ET 101")
 for j in range (10):
     D-=1
     if D==0:
-        break
-    b=int (input ("ENTREZ VOTRE VALUER : "))
-    if b== a : 
+        print (f"**VOUS AVEZ PERDU**     CHANCE RESTANTES=0 \n LA VALEUR A TROUVER EST {a}")
+        break  
+    b=int (input ("ENTREZ VOTRE VALUER : ")) 
+    if b== a :
+        print (f"**VOUS AVEZE GAGNEZ**     CHANCES RESTANTES : {D}") 
         break
     elif b<a :
+        print (f"{b} EST INFERIEUR     CHANCES RESTANTES : {D}")
         continue
     elif b>a  :
+        print (f"{b} EST SUPERIEUR     CHANCES RESTANTES : {D}")
         continue     
